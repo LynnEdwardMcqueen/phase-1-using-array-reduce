5e177b22da1ebee3e9c607cdc8987ebf0d476b41
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-04-01 20:13:01 -0600

    adds reduce furnctionality

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+let totalBatteries = 0;
+
 // Code your solution here
+function batteries(total, batchNumber) {
+
+    total += batchNumber;
+    totalBatteries = total;
+    debugger
+    return total;
+}
+
+console.log(batteryBatches.reduce(batteries, 0))
+console.log(totalBatteries);    
